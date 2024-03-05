0aae40137dd0c9896a13e02be1d372fdcb93180f
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2024-03-05 20:36:04 +0200

    user section done

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,16 +1,20 @@
 import styles from "./UserCard.module.scss";
 
 import { User } from "../../interfaces/interfaces";
+import { getTruncatedEmail, getEmailClass } from "../../helpers/emailUtils";
 
 export const UserCard = ({ user: { photo, position, name, email, phone } }: { user: User }) => {
+  const emailClass = getEmailClass(email);
+  const truncatedEmail = getTruncatedEmail(email);
+
   return (
     <li className={styles.userCard}>
       <img className={styles.userAvatar} width={70} height={70} src={photo} alt={name} />
       <p className={styles.userName}>{name}</p>
       <div>
         <p>{position}</p>
-        <a className={`${styles.tooltip} ${styles.title}`} href={`mailto:${email}`} title={email}>
-          {email}
+        <a className={emailClass} href={`mailto:${email}`} title={email}>
+          {truncatedEmail}
         </a>
         <a className={styles.title} href={`tel:${phone}`}>
           {phone}
--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = "https://frontend-test-assignment-api.abz.agency/api/v1/users";
+export const LENGTH_TO_TRIM = 25;
